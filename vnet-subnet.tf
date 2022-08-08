fb3ab4ed105e66fb2993f1bd17dcd76547fa3919
milan ramanayake <milan.sr@live.com>
2022-08-07 20:50:29 -0400

    third commit

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -10,7 +10,9 @@ resource "azurerm_subnet" "myterraformsubnet" {
     name = var.subnetname
     resource_group_name = azurerm_resource_group.RG65.name
     virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
-    address_prefix = ["10.0.1.0/24"]
+
+    address_prefixes = [ "10.0.2.0/24" ]
+    tags = var.tags
     
 }
   
