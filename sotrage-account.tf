36dbb762da7abab4c6c44c3b74a0a1b004e73220
milan ramanayake <milan.sr@live.com>
2022-08-08 16:20:03 -0400

    add vm commit

--- sotrage-account.tf
+++ sotrage-account.tf
@@ -1,4 +1,4 @@
-resource "random_id"  "random_id" {
+resource "random_id"  "randomid" {
   keepers = {
     resource_group = azurerm_resource_group.RG65.name
   }
@@ -6,7 +6,7 @@ resource "random_id"  "random_id" {
 }
 
 resource "azurerm_storage_account" "mystorageaccount" {
-  name                     = "diag${random_id.random_Id.hex}"
+  name                     = "diag${random_id.randomid.hex}"
   resource_group_name      = azurerm_resource_group.RG65.name
   location                 = var.location
   account_tier             = "Standard"
